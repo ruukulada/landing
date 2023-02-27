a6782ed148af4146f4a499310a8e145e2853f98a
ruukulada <iamfromthefuture42@gmail.com>
2023-02-26 20:53:43 -0500

    Styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     declarations: [
@@ -16,8 +23,15 @@ import { MatCardModule } from '@angular/material/card';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatToolbarModule,
-        MatCardModule
+        MatBadgeModule,
+        MatButtonModule,
+        MatCardModule,
+        MatDividerModule,
+        MatIconModule,
+        MatListModule,
+        MatMenuModule,
+        MatProgressSpinnerModule,
+        MatToolbarModule
     ],
     providers: [],
     bootstrap: [AppComponent]
